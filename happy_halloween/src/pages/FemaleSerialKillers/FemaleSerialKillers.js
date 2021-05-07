7016cfcd64a45de52059c4e7f8a758a0e166264f
Sonia <st.remysonia@gmail.com>
2021-05-06 23:38:21 -0400

    deleted all the profile images and replace them with url addresses

--- happy_halloween/src/pages/FemaleSerialKillers/FemaleSerialKillers.js
+++ happy_halloween/src/pages/FemaleSerialKillers/FemaleSerialKillers.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FemaleSerialKillers.css'
-import Cards from '../../components/Cards/Cards'
+import FemaleCards from '../../components/FemaleCards/FemaleCards'
 
 const FemaleSerialKillers = () => {
+    const [data , setData] = useState([])
+        
+
+        const getData=()=>{
+            fetch('SerialKillerApi.json'
+            ,{
+              headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+               }
+            }
+            )
+              .then(function(response){
+                console.log(response)
+                return response.json();
+              })
+              .then(function(myJson) {
+                console.log(myJson);
+                setData(myJson);
+              }).catch((error)=> {
+                  console.log(error)
+              })
+            
+
+          } 
+          useEffect(()=> {
+              getData()
+          }, [])
         return (
             <>
-                <Cards></Cards>
+                <FemaleCards killerdata={data}/>
             </>
     );
 }
